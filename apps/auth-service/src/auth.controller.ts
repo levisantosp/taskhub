9a7311c869d58a3daf410761a938bd69a0d0ed03
levisantosp <contatowizard01@gmail.com>
2025-10-15 16:09:30 -0300

    refactor: use `@Body()` decorator and destructuring for payload handling

--- apps/auth-service/src/auth.controller.ts
+++ apps/auth-service/src/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
 import { RegisterDto } from './dto/register.dto.ts'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from './auth.service.ts'
@@ -9,17 +9,17 @@ export class AuthController {
     public constructor(private readonly authService: AuthService) {}
 
     @Post('register')
-    async register(registerDto: RegisterDto) {
-        return this.authService.register(registerDto)
+    async register(@Body() register: RegisterDto) {
+        return this.authService.register(register)
     }
 
     @Post('login')
-    async login(@Body() loginDto: LoginDto) {
-        return this.authService.login(loginDto)
+    async login(@Body() login: LoginDto) {
+        return this.authService.login(login)
     }
 
     @Post('refresh')
-    async refresh(refreshToken: string) {
+    async refresh(@Body() { refreshToken }: { refreshToken: string }) {
         return this.authService.refreshToken(refreshToken)
     }
 
