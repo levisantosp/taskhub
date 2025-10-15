a71a8ef759b2c450b48c6f2d347a8f98539db992
levisantosp <contatowizard01@gmail.com>
2025-10-15 16:16:37 -0300

    feat: add changedBy

--- apps/tasks-service/src/dto/update-task.dto.ts
+++ apps/tasks-service/src/dto/update-task.dto.ts
@@ -1,8 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { CreateTask } from './create-task.dto.ts'
 import { IsEnum } from 'class-validator'
+import { Column } from 'typeorm'
 
 export class UpdateTask extends PartialType(CreateTask) {
     @IsEnum(['TODO', 'IN_PROGRESS', 'REVIEW', 'DONE'])
     public status!: 'TODO' | 'IN_PROGRESS' | 'REVIEW' | 'DONE'
+
+    @Column('uuid')
+    public changedBy!: string
 }
