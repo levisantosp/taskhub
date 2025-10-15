f1c1395e97f6bbdc023481aaae681aba624fe637
levisantosp <contatowizard01@gmail.com>
2025-10-15 03:09:47 -0300

    feat: initialize tasks service

--- apps/tasks-service/src/index.ts
+++ apps/tasks-service/src/index.ts
@@ -1,3 +1,22 @@
+import 'reflect-metadata'
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { TasksModule } from './tasks.module.ts'
 import { info } from '@taskhub/utils'
 
-info('hello world')
+const app = await NestFactory.create(TasksModule)
+
+app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true
+    })
+)
+    .enableCors()
+
+const port = process.env.PORT || 3003
+
+await app.listen(port)
+
+info(`tasks service running at ${port}`)
