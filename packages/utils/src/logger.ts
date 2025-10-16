def469a8376e8d7b839ebbe6ebc341199732d33d
levisantosp <contatowizard01@gmail.com>
2025-10-15 23:51:49 -0300

    refactor: broaden logger functions to accept unknown type

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -1,8 +1,8 @@
 import colors from 'colors'
 import moment from 'moment'
 
-export const info = (message: string) => console.log(colors.green(`[${moment(Date.now()).format('hh:mm:ss A')}] ${message}`))
+export const info = (message: unknown) => console.log(colors.green(`[${moment(Date.now()).format('hh:mm:ss A')}] ${message}`))
 
-export const warn = (message: string) => console.log(colors.yellow(`[${moment(Date.now()).format('hh:mm:ss A')}] ${message}`))
+export const warn = (message: unknown) => console.log(colors.yellow(`[${moment(Date.now()).format('hh:mm:ss A')}] ${message}`))
 
 export const error = (error: Error) => console.log(colors.red(`[${moment(Date.now()).format('hh:mm:ss A')}] ${error.stack ?? error}`))
