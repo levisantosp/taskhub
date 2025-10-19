796df0fe7d4a7ab8ab7e5882dc7214708d873710
levisantosp <contatowizard01@gmail.com>
2025-10-19 20:14:33 -0300

    feat(api-gateway): add create task dto

---
+++ apps/api-gateway/src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, IsOptional, IsEnum, IsArray, IsDateString, IsUUID } from 'class-validator'
+
+export class CreateTask {
+    @IsString()
+    public title!: string
+
+    @IsString()
+    @IsOptional()
+    public description?: string
+
+    @IsDateString()
+    public deadline!: Date
+
+    @IsEnum(['LOW', 'MEDIUM', 'HIGH', 'URGENT'])
+    @IsOptional()
+    public priority?: 'LOW' | 'MEDIUM' | 'HIGH' | 'URGENT'
+
+    @IsArray()
+    @IsOptional()
+    public assignedUserIds?: string[]
+
+    @IsUUID()
+    public createdBy!: string
+}
