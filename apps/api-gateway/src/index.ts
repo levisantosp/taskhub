c519c4e066ec4cb947e74e1946322b27f1d0f9ad
levisantosp <contatowizard01@gmail.com>
2025-10-18 22:20:46 -0300

    docs: add swagger api documentation

--- apps/api-gateway/src/index.ts
+++ apps/api-gateway/src/index.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { info } from '@taskhub/utils'
 import { AppModule } from './app.module.ts'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 const app = await NestFactory.create(AppModule)
+
+const doc = new DocumentBuilder()
+    .setTitle('TaskHub API')
+    .setDescription('A collaborative task management platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+SwaggerModule.setup('/api/docs', app, SwaggerModule.createDocument(app, doc))
 
 const port = process.env.PORT || 3001
 
@@ -14,3 +24,4 @@ app.enableCors({
 await app.listen(port)
 
 info(`api gateway running at ${port}`)
+info(`swagger avaialble at http://localhost:${port}/api/docs`)
