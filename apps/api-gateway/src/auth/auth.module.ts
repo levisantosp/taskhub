06295e18b71e1569592be484d5b373d67a1be001
levisantosp <contatowizard01@gmail.com>
2025-10-22 11:18:04 -0300

    feat(gateway): configure jwt module for validation and export guard

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -3,10 +3,24 @@ import { AuthController } from './auth.controller.ts'
 import { AuthClient } from './auth-client.ts'
 import { JwtStrategy } from './jwt.strategy.ts'
 import { JwtAuthGuard } from './jwt-auth.guard.ts'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
     controllers: [AuthController],
     providers: [AuthClient, JwtStrategy, JwtAuthGuard],
-    exports: [AuthClient]
+    exports: [JwtAuthGuard],
+    imports: [
+        ConfigModule.forRoot(),
+        PassportModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async(config: ConfigService) => ({
+                secret: config.get<string>('JWT_ACCESS_TOKEN')
+            })
+        })
+    ]
 })
 export class AuthModule {}
