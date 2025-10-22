1bbac0fb63cef8b43624bd8ada2176fc03d2d600
levisantosp <contatowizard01@gmail.com>
2025-10-22 11:24:46 -0300

    feat(gateway): add validation and swagger docs to create-comment dto

--- apps/api-gateway/src/tasks/dto/create-comment.dto.ts
+++ apps/api-gateway/src/tasks/dto/create-comment.dto.ts
@@ -1,9 +1,20 @@
-import { IsString, IsUUID } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, IsUUID, MinLength } from 'class-validator'
 
 export class CreateComment {
+    @ApiProperty({
+        description: 'Comment content',
+        example: 'This needs to be reviewed'
+    })
     @IsString()
+    @IsNotEmpty()
+    @MinLength(1)
     public content!: string
 
+    @ApiProperty({
+        description: 'Author ID'
+    })
+    @IsNotEmpty()
     @IsUUID()
     public authorId!: string
 }
