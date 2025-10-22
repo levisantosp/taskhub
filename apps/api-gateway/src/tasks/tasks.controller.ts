b545da9a5ce59c78dfc32b772fc5506af05e7ab0
levisantosp <contatowizard01@gmail.com>
2025-10-22 11:23:06 -0300

    feat(gateway): secure tasks controller, add comments endpoint, and implement dtos

--- apps/api-gateway/src/tasks/tasks.controller.ts
+++ apps/api-gateway/src/tasks/tasks.controller.ts
@@ -1,8 +1,29 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Inject,
+    Param,
+    ParseUUIDPipe,
+    Post,
+    Put,
+    Query,
+    Request,
+    UseGuards
+} from '@nestjs/common'
 import { TasksClient } from './tasks-client.ts'
 import { firstValueFrom } from 'rxjs'
+import { JwtAuthGuard } from '../auth/jwt-auth.guard.ts'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { CreateComment } from './dto/create-comment.dto.ts'
+import { CreateTask } from './dto/create-task.dto.ts'
+import { UpdateTask } from './dto/update-task.dto.ts'
 
+@ApiTags('Tasks')
+@ApiBearerAuth()
 @Controller('tasks')
+@UseGuards(JwtAuthGuard)
 export class TasksController {
     public constructor(private readonly task: TasksClient) {}
 
@@ -11,57 +32,36 @@ export class TasksController {
         @Query('page') page = 1,
         @Query('size') size = 10
     ) {
-        try {
-            return await firstValueFrom(this.task.send('find.tasks', { page, size }))
-        }
-
-        catch(e) {
-            throw new Error((e as Error).message)
-        }
+        return await firstValueFrom(this.task.send('find.tasks', { page, size }))
     }
 
     @Get(':id')
     public async getTask(@Param('id') id: string) {
-        try {
-            return await firstValueFrom(this.task.send('get.task', { id }))
-        }
-
-        catch(e) {
-            throw new Error((e as Error).message)
-        }
+        return await firstValueFrom(this.task.send('find.task', { id }))
     }
 
     @Post()
-    public async createTask(@Body() data: unknown) {
-        try {
-            return await firstValueFrom(this.task.send('create.task', data))
-        }
-
-        catch(e) {
-            throw new Error((e as Error).message) 
-        }
+    public async createTask(
+        @Body() data: CreateTask,
+        @Request() req: any
+    ) {
+        return await firstValueFrom(this.task.send('create.task', {
+            ...data,
+            createdBy: req.user.userId
+        }))
     }
 
     @Put(':id')
-    public async updateTask(@Param('id') id: string, @Body() data: any) {
-        try {
-            return await firstValueFrom(this.task.send('update.task', { id, ...data }))
-        }
-
-        catch(e) {
-            throw new Error((e as Error).message) 
-        }
+    public async updateTask(
+        @Param('id') id: string,
+        @Body() data: UpdateTask
+    ) {
+        return await firstValueFrom(this.task.send('update.task', { id, ...data }))
     }
 
     @Delete(':id')
     public async deleteTask(@Param('id') id: string) {
-        try {
-            return await firstValueFrom(this.task.send('delete.task', { id }))
-        }
-
-        catch(e) {
-            throw new Error((e as Error).message) 
-        }
+        return await firstValueFrom(this.task.send('delete.task', { id }))
     }
 
     @Get(':id/comments')
@@ -70,12 +70,19 @@ export class TasksController {
         @Query('page') page = 1,
         @Query('size') size = 10
     ) {
-        try {
-            return await firstValueFrom(this.task.send('get.comments', { id, page, size }))
-        }
+        return await firstValueFrom(this.task.send('find.comments', { id, page, size }))
+    }
 
-        catch(e) {
-            throw new Error((e as Error).message) 
-        }
+    @Post(':id/comments')
+    public async createComment(
+        @Param('id', ParseUUIDPipe) taskId: string,
+        @Body() comment: CreateComment,
+        @Request() req: any
+    ) {
+        return await firstValueFrom(this.task.send('task.create.comment', {
+            ...comment,
+            createdBy: req.user.userId,
+            taskId
+        }))
     }
 }
