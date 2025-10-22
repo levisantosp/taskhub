e5387e44fbe8955e3ed96a05250f7c1c112ba54e
levisantosp <contatowizard01@gmail.com>
2025-10-22 11:15:36 -0300

    feat(gateway): implement global rate limiting and provide websocket gateway

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module.ts'
 import { TasksModule } from './tasks/tasks.module.ts'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core'
+import { ApiWebSocketGateWay } from './ws/websocket.gateway.ts'
 
 @Module({
-    imports: [AuthModule, TasksModule]
+    imports: [
+        AuthModule,
+        TasksModule,
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    ttl: 1,
+                    limit: 10
+                }
+            ]
+        })
+    ],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard
+        },
+        ApiWebSocketGateWay
+    ]
 })
 export class AppModule {}
