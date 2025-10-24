93351d61ef86bf75e51bc400ba0d15a4c6a1783b
levisantosp <levi.pires2005@gmail.com>
2025-10-24 15:28:23 -0300

    feat(gateway): add validation messages and rules to LoginDto

--- apps/api-gateway/src/auth/dto/login.dto.ts
+++ apps/api-gateway/src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
-import { IsEmail, IsString } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginDto {
-    @IsEmail()
+    @IsEmail(undefined, {
+        message: 'Please provide an valid email address'
+    })
     public email!: string
 
     @IsString()
+    @IsNotEmpty({ message: 'Password should not be empty' })
     public password!: string
 }
