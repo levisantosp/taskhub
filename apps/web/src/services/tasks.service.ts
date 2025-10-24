f2ca0bf7c164d5aef72b864a00f85056b0c3767f
levisantosp <levi.pires2005@gmail.com>
2025-10-24 15:39:32 -0300

    feat(web): implement search and filter parameters in getTasks service method

--- apps/web/src/services/tasks.service.ts
+++ apps/web/src/services/tasks.service.ts
@@ -18,17 +18,35 @@ type ApiCommentsResponse = {
 }
 
 export const taskService = {
-    getTasks: async(page = 1, size = 10) =>
-        await apiClient.get<ApiTasksResponse>(`/tasks?page=${page}&size=${size}`),
-    getTask: async(id: string) =>
+    getTasks: async (
+        page = 1,
+        size = 10,
+        search = '',
+        status = 'all',
+        priority = 'all'
+    ) => {
+        const params = new URLSearchParams({
+            page: page.toString(),
+            size: size.toString()
+        })
+
+        if(search.length) params.append('search', search)
+
+        if(status !== 'all') params.append('status', status)
+
+        if(priority !== 'all') params.append('priority', priority)
+
+        return await apiClient.get<ApiTasksResponse>(`/tasks?${params.toString()}`)
+    },
+    getTask: async (id: string) =>
         await apiClient.get<Task>(`/tasks/${id}`),
-    createTask: async(data: unknown) =>
+    createTask: async (data: unknown) =>
         await apiClient.post('/tasks', data),
-    updateTask: async(id: string, data: unknown) =>
+    updateTask: async (id: string, data: unknown) =>
         await apiClient.put(`/tasks/${id}`, data),
-    deleteTask: async(id: string) =>
+    deleteTask: async (id: string) =>
         await apiClient.delete(`/tasks/${id}`),
-    getComments: async(taskId: string, page = 1, size = 10) =>
+    getComments: async (taskId: string, page = 1, size = 10) =>
         await apiClient.get<ApiCommentsResponse>(`/tasks/${taskId}/comments?page=${page}&size=${size}`),
     createComment: async<T extends string>(taskId: string, content: T) =>
         await apiClient.post(`/tasks/${taskId}/comments`, { content })
