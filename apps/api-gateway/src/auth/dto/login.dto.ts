41ff10b51c887025021f0bdff90fc8119e624939
levisantosp <levi.pires2005@gmail.com>
2025-10-25 20:13:25 -0300

    docs(gateway): add ApiProperty decorators to LoginDto

--- apps/api-gateway/src/auth/dto/login.dto.ts
+++ apps/api-gateway/src/auth/dto/login.dto.ts
@@ -1,12 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class LoginDto {
     @IsEmail(undefined, {
         message: 'Please provide an valid email address'
     })
+    @ApiProperty({
+        example: 'user@domain.com',
+        description: 'User email address'
+    })
     public email!: string
 
     @IsString()
     @IsNotEmpty({ message: 'Password should not be empty' })
+    @ApiProperty({
+        description: 'User password'
+    })
     public password!: string
 }
