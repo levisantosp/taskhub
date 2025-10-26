840469f619dffc9f94b016adcd93e0d55ae85e46
levisantosp <levi.pires2005@gmail.com>
2025-10-25 23:41:18 -0300

    minor changes

--- apps/web/src/context/auth-context.tsx
+++ apps/web/src/context/auth-context.tsx
@@ -66,7 +66,7 @@ export function AuthProvider({ children }: Props) {
 
         const currentPath = window.location.pathname
 
-        if(!token && (currentPath === '/tasks' || currentPath === '/')) {
+        if(!token && (currentPath.startsWith('/tasks') || currentPath === '/')) {
             navigate({ to: '/login' })
         }
     }, [navigate])
@@ -74,6 +74,8 @@ export function AuthProvider({ children }: Props) {
     const login = async(email: string, pass: string) => {
         try {
             const res = await apiClient.post<LoginRes>('/auth/login', { email, password: pass })
+
+            console.log(res)
 
             setUser(res.data.user)
 
